86210adbb8bc413ad09ed52338caefa88298d05b
Antonin Januska <antonin@antjanus.com>
2014-04-30 17:16:01 -0500

    Basic LED logic

--- torrentuino.js
+++ torrentuino.js
@@ -1,9 +1,39 @@
 var arduino = require('duino');
-var board = new arduino.Board();
+var board   = new arduino.Board();
+var _       = require('lodash');
 
-var led = new arduino.Led({
-    board: board,
-    pin: 13
+//setup
+
+var leds = _.range(4, 13);
+var ledObj = [];
+var infiniteLoop = true;
+
+leds.forEach(function(l) {
+    ledObj[] = new arduino.Led({
+        board: board,
+        pin: l
+    });
 });
 
-led.blink();
+var resetLeds = function() {
+    for(var i = 0; i =< ledObj.length; i++) {
+        ledObj[i].off();
+    }
+};
+
+//get data
+
+while(infiniteLoop) {
+    //get data - let's say we get 50% back
+    var data = 50;
+    var completedBlock = Math.floor(data / 10);
+
+    resetLeds();
+
+    //turn on correct LEDs
+    for var i = 0; i < completedBlock; i++) {
+        ledObj[i].on();
+    }
+
+    ledObj[completedBlock + 1].blink();
+};
